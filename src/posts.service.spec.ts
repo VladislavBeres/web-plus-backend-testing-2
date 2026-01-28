622eb107ee08082ee40d59116e1c828ba4a301b1
Владислав <vadislavberestyuk@yandex.ru>
2026-01-28 15:12:01 +0300

    Юнит тесты задание 2 версия 1.0.0

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,32 +1,67 @@
-import { PostsService } from './posts.service';
+import { PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
 
   beforeEach(() => {
     postsService = new PostsService();
   });
 
-  describe('.findMany', () => {
+  describe(".findMany", () => {
     const posts = [
-      {text: 'Post 1'},
-      {text: 'Post 2'},
-      {text: 'Post 3'},
-      {text: 'Post 4'},
+      { text: "Post 1" },
+      { text: "Post 2" },
+      { text: "Post 3" },
+      { text: "Post 4" },
     ];
 
     beforeEach(() => {
       posts.forEach((post) => postsService.create(post));
     });
 
-    it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+    it("should return all posts if called without options", () => {
+      const foundPosts = postsService.findMany();
+
+      expect(foundPosts).toHaveLength(posts.length);
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(posts.map(expect.objectContaining)),
+      );
     });
 
-    it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+    it("should return correct posts for skip and limit options", () => {
+      const limit = 2;
+      const skip = 1;
+
+      const foundPosts = postsService.findMany({ skip, limit });
+
+      expect(foundPosts).toHaveLength(limit);
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(
+          posts.slice(skip, limit).map(expect.objectContaining),
+        ),
+      );
     });
 
-    // реализуйте недостающие тест-кейсы
+    it("should return correct posts for skip option", () => {
+      const skip = 2;
+      const foundPosts = postsService.findMany({ skip });
+
+      expect(foundPosts).toHaveLength(posts.length - skip);
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(posts.slice(skip).map(expect.objectContaining)),
+      );
+    });
+
+    it("should return correct posts for limit option", () => {
+      const limit = 3;
+      const foundPosts = postsService.findMany({ limit });
+
+      expect(foundPosts).toHaveLength(limit);
+      expect(foundPosts).toEqual(
+        expect.arrayContaining(
+          posts.slice(0, limit).map(expect.objectContaining),
+        ),
+      );
+    });
   });
 });
